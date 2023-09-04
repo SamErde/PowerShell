60fde5250e6f9ef49fe94ee50799a4bb8a80b955
Sam Erde <samuel.erde@gmail.com>
2023-09-04 06:32:41 -0400

    Upload more notes and scripts

---
+++ Snippets/Get Script Path, Name.ps1
@@ -0,0 +1,59 @@
+Function Get-ScriptPath {
+    <#
+    .SYNOPSIS
+        Get-ScriptPath returns the path of the current script.
+    .OUTPUTS
+        System.String
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param()
+
+    Begin {
+        Remove-Variable ScriptPath
+    }
+    Process {
+        If ($psEditor) { Split-Path -Path $psEditor.GetEditorContext().CurrentFile.Path } # Visual Studio Code
+        ElseIf ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") { Split-Path -Path $My$MyInvocation.MyCommand.Source } # PS1 converted to EXE
+        ElseIf ($null -ne $HostInvocation) { $HostInvocation.MyCommand.Path } # SAPIEN PowerShell Studio
+        ElseIf ($psISE) { Split-Path -Path $psISE.CurrentFile.FullPath } # Windows PowerShell ISE
+        ElseIf ($MyInvocation.PSScriptRoot) { $MyInvocation.PSScriptRoot } # Windows PowerShell 3.0+
+        ElseIf ($MyInvocation.MyCommand.Path) { Split-Path -Path $MyInvocation.MyCommand.Path -Parent } # Windows PowerShell
+        Else {
+            Write-Host -Object "Unable to resolve script's file path!" -ForegroundColor Red
+            Exit 1
+        }
+    }
+}
+
+Function Get-ScriptName {
+    <#
+    .SYNOPSIS
+        Get-ScriptName returns the name of the current script.
+    .OUTPUTS
+        System.String
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param()
+    Begin {
+        Remove-Variable ScriptName
+    }
+    Process {
+        If ($psEditor) { Split-Path -Path $psEditor.GetEditorContext().CurrentFile.Path -Leaf } # Visual Studio Code Host
+        ElseIf ($psEXE) { [System.Diagnotics.Process]::GetCurrentProcess.Name } # PS1 converted to EXE
+        ElseIf ($null -ne $HostInvocation) { $HostInvocation.MyCommand.Name } # SAPIEN PowerShell Studio
+        ElseIf ($psISE) { $psISE.CurrentFile.DisplayName.Trim("*") } # Windows PowerShell ISE
+        ElseIf ($MyInvocation.MyCommand.Name) { $MyInvocation.MyCommand.Name } # Windows PowerShell
+        Else {
+            Write-Host -Object "Uanble to resolve script's file name!" -ForegroundColor Red
+            Exit 1
+        }
+    }
+}
+
+[string]$ScriptPath = Get-ScriptPath # Get the current script path
+[string]$ScriptName = Get-ScriptName # Get the current script name
+
+$ScriptPath
+$ScriptName
---
+++ Snippets/Suppress Output.ps1
@@ -0,0 +1,4 @@
+$null = Invoke-Something
+[void](Invoke-Something)
+Invoke-Something > $null
+Invoke-Something | Out-Null
