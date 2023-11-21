fa10fb706f2b3a7968c1cf56a2bef9e2b0e6f480
Sam Erde <20478745+SamErde@users.noreply.github.com>
2023-11-21 14:36:50 -0500

    Refactoring code

--- Copy-ToPSSession.ps1
+++ Copy-ToPSSession.ps1
@@ -1,34 +1,35 @@
 <#
 .SYNOPSIS
-Copy files to a server using PowerShell Sessions instead of SMB
+Copy files to a server using PowerShell remoting sessions instead of SMB.
 
 .DESCRIPTION
-A method to copy files to a server using PowerShell remoting sessions instead of SMB, which should be off/blocked 
-as a best practice in secure networks.
+A method to copy files to a server using PowerShell remoting sessions instead of SMB, 
+which should be off/blocked as a best practice in secure networks.
 
 .NOTES
 The parameters below could be setup to accept objects from the pipeline.
 #>
 
-function Copy-ToPSSession {
+function Copy-ItemToRemote {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [string]
-        $TargetServer,
+            [string]$RemoteComputer,
         [Parameter()]
-        [string]
-        $SourcePath,
+            [string]$SourcePath,
         [Parameter()]
-        [string]
-        $DestinationPath
+            [string]$DestinationPath,
+        [Parameter(Mandatory=$false)]
+            $Session
     )
 
-    $Session = New-PSSession -ComputerName $TargetServer
+    if (-not $Session) {
+        $Session = New-PSSession -ComputerName $RemoteComputer #-Credential
+    }
     try {
-    Copy-Item -Path $SourcePath -Target $DestinationPath -ToSession $Session
+        Copy-Item -Path $SourcePath -Target $DestinationPath -ToSession $Session
     }
     catch {
-    $Error
+        $Error
     }
 }
