4a8909f449df750e0386339c1cf5d1cbd63e77fc
Sam Erde <samuel.erde@gmail.com>
2024-03-01 10:01:23 -0500

    Create new script

---
+++ Set-EnvironmentVariable.ps1
@@ -0,0 +1,31 @@
+function Set-EnvironmentVariable {
+    [Alias("sev")]
+    [CmdletBinding()]
+    param (
+        # The name of the environment variable to set.
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        # The value of environment variable to set.
+        [Parameter(Mandatory)]
+        [string]
+        $Value,
+
+        # The target of the environment variable to set.
+        [Parameter()]
+        [System.EnvironmentVariableTarget]
+        $Target
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        [Environment]::SetEnvironmentVariable($Name, $Value, $Target)
+    }
+    
+    end {
+        
+    }
+}
