0b7800fcd4e6f894cf05f8c0fd4059176c658e17
Sam Erde <samuel.erde@gmail.com>
2024-06-17 15:02:45 -0400

    Fix eq operator

--- Snippets/Check if a key exists in a hash table.ps1
+++ Snippets/Check if a key exists in a hash table.ps1
@@ -1,6 +1,6 @@
 # Check if a string exists as a key in a hash table
 $StringToCheck = "SomeString"
-if ($null = $HashTable[$StringToCheck]) {
+if ($null -eq $HashTable[$StringToCheck]) {
     Write-Output "The key [$StringToCheck] does not exist in the hash table"
 }
 else {
