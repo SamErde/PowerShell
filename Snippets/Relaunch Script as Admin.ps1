4593c48530e062aa5abe687c0fb805dd470c2806
Sam Erde <samuel.erde@gmail.com>
2024-07-10 10:57:34 -0400

    Exchange Server and Exchange Online

---
+++ Snippets/Relaunch Script as Admin.ps1
@@ -0,0 +1,18 @@
+$isAdmin = [System.Security.Principal.WindowsPrincipal]::new(
+    [System.Security.Principal.WindowsIdentity]::GetCurrent()).
+        IsInRole('Administrators')
+
+if(-not $isAdmin) {
+    $params = @{
+        FilePath     = 'powershell' # or pwsh if Core
+        Verb         = 'RunAs'
+        ArgumentList = @(
+            '-NoExit'
+            '-ExecutionPolicy ByPass'
+            '-File "{0}"' -f $PSCommandPath
+        )
+    }
+
+    Start-Process @params
+    return
+}
