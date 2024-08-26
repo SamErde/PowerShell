67ddf450aebb5fd5669a24d52890f2b645c0e6c6
Sam Erde <samuel.erde@gmail.com>
2024-08-26 14:26:46 -0400

    Prompt

--- Profile and Prompt/Simple prompt with history.ps1
+++ Profile and Prompt/Simple prompt with history.ps1
@@ -1,6 +1,13 @@
-function Prompt
-{
-    Write-Host ("PS " + $(get-date) + " [$((Get-History).Count)] " + "$pwd>") #-ForegroundColor White
-    Write-Host ">" -NoNewline #-ForegroundColor White
-    return " "
+function PromptPlain {
+    Write-Host ('PS ' + $(Get-Date) + " [$((Get-History).Count)] " + "$pwd>") #-ForegroundColor White
+    Write-Host '>' -NoNewline #-ForegroundColor White
+    return ' '
+}
+
+function Prompt {
+    Write-Host "[$((Get-History).Count)] " -NoNewline -ForegroundColor Cyan
+    Write-Host "$([math]::Ceiling((((Get-History)[-1]).EndExecutionTime - ((Get-History)[-1]).StartExecutionTime).TotalMilliseconds))ms " -NoNewline -ForegroundColor Yellow
+    Write-Host "$($PWD.ToString().Replace($HOME,'~'))" -ForegroundColor Cyan
+    Write-Host '>' -NoNewline -ForegroundColor White
+    return ' '
 }
