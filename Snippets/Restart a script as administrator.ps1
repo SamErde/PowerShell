316e56e1a32dc07fadbf06709589b81c7f13964c
Sam Erde <samuel.erde@gmail.com>
2024-08-26 14:26:31 -0400

    Snippet

---
+++ Snippets/Restart a script as administrator.ps1
@@ -0,0 +1,4 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
+        [Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -Wait
+}
