643e6791c2a0b97ab619c332c083d27ced417d1b
Sam Erde <samuel.erde@gmail.com>
2024-09-09 10:05:29 -0400

    Wrap link in GTLT

--- General/Get-EnvironmentVariable.ps1
+++ General/Get-EnvironmentVariable.ps1
@@ -30,8 +30,8 @@ function Get-EnvironmentVariable {
             The relevant sources for the User and Machine targets are in the registry at:
             - HKEY_CURRENT_USER\Environment
             - HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
-        .LINK
-            https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_environment_variables
+
+            Read more at <https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_environment_variables>.
     #>
     [Alias('gev')]
     [OutputType([System.String], [System.Collections.IDictionary])]
