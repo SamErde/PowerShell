256d0d3ec2e032e574c8811ad6c6263e8734df47
Sam Erde <samuel.erde@gmail.com>
2024-09-18 10:23:17 -0400

    Script for potential GH actions

---
+++ .github/workflow-scripts/Get-RepositoryInfo.ps1
@@ -0,0 +1,18 @@
+function Get-RepositoryInfo {
+    [CmdletBinding()]
+    param (
+    )
+
+    $RepositoryRoot = [System.IO.Path]::Combine($PSScriptRoot, '..', '..')
+
+    [PSCustomObject]$Info = @{
+        NumberOfScripts = (Get-ChildItem $RepositoryRoot -Filter *.ps1 -File -Recurse).Count
+        NumberOfFOlders = (Get-ChildItem $RepositoryRoot -Directory -Recurse).Count
+        Categories      = (Get-ChildItem -Path $RepositoryRoot -Directory -Exclude '.github').Name
+        CategoryList    = (Get-ChildItem -Path $RepositoryRoot -Directory -Exclude '.github').Name -replace 'DDI', 'DDI (DNS, DHCP, IPAM)' -join ', '
+    }
+
+    $Info
+}
+
+Get-RepositoryInfo
