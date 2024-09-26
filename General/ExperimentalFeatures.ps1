dc8add13194b01898d724e155af13c61bfc9dd07
Sam Erde <samuel.erde@gmail.com>
2024-09-26 14:03:02 -0500

    Add

---
+++ General/ExperimentalFeatures.ps1
@@ -0,0 +1,2 @@
+Get-ExperimentalFeature | Sort-Object Enabled, Name | Format-Table Name, Enabled, Description
+Write-Output 'See <https://learn.microsoft.com/en-us/powershell/scripting/learn/experimental-features#available-features> for more information about experimental features in PowerShell.'
