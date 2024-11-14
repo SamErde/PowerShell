112c8e17e4a69ab56e4a4c8f0e313b5b86b1a655
Sam Erde <samuel.erde@gmail.com>
2024-11-14 15:14:06 -0500

    SID History Functions

---
+++ Active Directory/Get-AllADSIDHistoryDetails.ps1
@@ -0,0 +1,44 @@
+﻿function Get-AllADSIDHistoryDetails {
+    [CmdletBinding()]
+    param ()
+
+    begin {
+        if (-not (Get-Module -Name ActiveDirectory)) {
+            Write-Verbose -Message 'Importing ActiveDirectory module.'
+            Import-Module ActiveDirectory
+            Write-Verbose -Message '------------------------------'
+            Write-Verbose -Message "Beginning ${MyInvocation.InvocationName}..."
+        }
+
+        $DomainSIDMapping = Get-TrustedDomainSIDMapping
+        $SourceDomainAssociatedObjects = @{}
+    } # end begin
+
+    process {
+        # Get all Active Directory objects that have a value in SID history.
+        $ADObjectsWithSIDHistory = Get-ADObject -Filter 'SIDHistory -like "*"' -Properties SIDHistory
+
+        # Loop through each ADObject and loop through each SID history item on that object.
+        # For each SID history item, remove the RID and add the domain SID to the $SourceDomains hash table.
+        foreach ($ADObject in $ADObjectsWithSIDHistory) {
+            foreach ($SIDHistory in $ADObject.SIDHistory) {
+                $DomainSID = $SIDHistory.Substring(0, $SIDHistory.LastIndexOf('-'))
+                # If the domain SID is in the $DomainSIDMapping hash table, use the domain name as the key. If not, use the DomainSID as the key in the $SourceDomains hash table.
+                $Domain = if ($DomainSIDMapping[$DomainSID]) {
+                    $DomainSIDMapping[$DomainSID].Value
+                } else {
+                    $DomainSID
+                }
+                # Add the domain name as the key and the ADObject as the value to the $SourceDomainAssociatedObjects hash table.
+                $SourceDomainAssociatedObjects.Add(
+                    $Domain,
+                    ([System.Collections.Generic.List[object]]).Add($ADObject)
+                )
+            }
+        }
+    } # end process
+
+    end {
+        $SourceDomainAssociatedObjects
+    }
+}
---
+++ Active Directory/Get-AllADSIDHistorySourceDomains.ps1
@@ -0,0 +1,46 @@
+﻿function Get-AllADSIDHistorySourceDomains {
+    <#
+    .SYNOPSIS
+    Get a list of source domains from all Active Directory objects that have SID history.
+
+    .DESCRIPTION
+    This function gets all Active Directory objects that have SID history and returns a list of source domain SIDs.
+
+    .EXAMPLE
+    Get-AllAdSidHistorySourceDomains
+
+    .NOTES
+    Author: Sam Erde, Sentinel Technologies, Inc.
+    Version: 0.0.1
+    Modified: 2024-11-14
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+        if (-not (Get-Module -Name ActiveDirectory)) {
+            Write-Verbose -Message 'Importing ActiveDirectory module.'
+            Import-Module ActiveDirectory
+            Write-Verbose -Message '------------------------------'
+            Write-Verbose -Message "Beginning ${MyInvocation.InvocationName}..."
+        }
+
+        $DomainSIDs = New-Object -TypeName System.Collections.Generic.List[System.String]
+    } # end begin
+
+    process {
+        # Get all ActiveDirectory objects that have SID history.
+        $AllSIDHistory = Get-ADObject -Filter { SIDHistory -like '*' } -Properties SIDHistory | Select-Object -ExpandProperty SIDHistory
+
+        foreach ($SID in $AllSIDHistory) {
+            $DomainSIDs.Add($SID.Substring(0, $SID.LastIndexOf('-')))
+        }
+    } # end process
+
+    end {
+
+    } # end end
+
+} # end function
