27f93ffcefd39ac0b1574d4da1ea7e29991b1e23
Sam Erde <samuel.erde@gmail.com>
2024-12-16 15:05:56 -0500

    Add script

---
+++ General/Test-IsPalindrome.ps1
@@ -0,0 +1,46 @@
+﻿function Test-IsPalindrome {
+    <#
+    .SYNOPSIS
+    Test a string to see if it is a palindrome.
+
+    .DESCRIPTION
+    This function tests a string to see if it is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.
+
+    .PARAMETER String
+    The string to test.
+
+    .EXAMPLE
+    Test-IsPalindrome -String "racecar"
+
+    The string "racecar" is a palindrome, so the function returns $true.
+
+    .EXAMPLE
+    '() ()' | Test-IsPalindrome
+
+    The string "() ()" is not a palindrome, so the function returns $false.
+
+    .EXAMPLE
+    '() )(' | Test-IsPalindrome
+
+    The string "() )(" is a palindrome, so the function returns $true.
+    #>
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param (
+        # The string to test
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $String
+    )
+
+    process {
+        $ReversedString = -join ($String.ToLower().ToCharArray() | ForEach-Object { $_ })[-1.. - ($String.Length)]
+        "`n$String    ||    $ReversedString" | Write-Verbose
+        $Result = $String -eq $ReversedString
+    }
+
+    end {
+        $Result
+    }
+}
