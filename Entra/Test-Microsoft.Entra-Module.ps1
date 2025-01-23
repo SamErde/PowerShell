a6b2ebcb807e3e207399b22a0a84ba8785b5798a
Sam Erde <samuel.erde@gmail.com>
2025-01-23 15:41:40 -0800

    Add script

---
+++ Entra/Test-Microsoft.Entra-Module.ps1
@@ -0,0 +1,11 @@
+﻿Get-Module -ListAvailable -Refresh -Name Microsoft.Entra* | Format-Table Name, RequiredModules
+
+Write-Host ("{0} seconds:`tList commands with '`Get-Command -Module Microsoft.Entra.*' `n" -f [math]::Round( (Measure-Command {
+    Get-Command -Module Microsoft.Entra.*
+} ).TotalSeconds, 2))
+
+@('Applications', 'Authentication', 'DirectoryManagement', 'Governance', 'Groups', 'Reports', 'SignIns', 'Users') | ForEach-Object {
+    Write-Host ("{0} seconds:`tMicrosoft.Entra.$_" -f [math]::Round((Measure-Command {
+        Import-Module -Name "Microsoft.Entra.$_"
+    }).TotalSeconds), 2)
+}
