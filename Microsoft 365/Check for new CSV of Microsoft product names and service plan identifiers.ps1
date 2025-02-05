ad53ee6a7d3c2600977aba0fa50d1e40c22d0734
Sam Erde <samuel.erde@gmail.com>
2025-02-04 23:59:55 -0500

    New script

---
+++ Microsoft 365/Check for new CSV of Microsoft product names and service plan identifiers.ps1
@@ -0,0 +1,4 @@
+﻿# Check Microsoft Learn for a new download of the Product Names and Service Plan Identifiers CSV file.
+$Response = Invoke-WebRequest -Uri 'https://learn.microsoft.com/en-us/entra/identity/users/licensing-service-plan-reference'
+$LicensingCsvDownloadLink = ( $Response.Links.Where({ $_.href -match 'licensing.csv' -or $_.href -match 'Product%20names%20and%20service%20plan' }) ).href
+$LicensingCsvDownloadLink
