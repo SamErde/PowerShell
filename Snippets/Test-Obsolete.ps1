6b9b0672134e2b8dadec8028455b6b8c949c2e98
Sam Erde <samuel.erde@gmail.com>
2025-02-12 15:52:55 -0500

    Clarify

--- Snippets/Test-Obsolete.ps1
+++ Snippets/Test-Obsolete.ps1
@@ -1,42 +1,32 @@
 function Test-Obsolete {
     <#
-        .SYNOPSIS
-        Test an obsolete parameter.
+    .SYNOPSIS
+    Test an obsolete parameter.
 
-        .DESCRIPTION
-        Test an obsolete parameter.
+    .DESCRIPTION
+    Test an obsolete parameter.
 
-        .EXAMPLE
-        Test-Obsolete -Mode 0
+    .EXAMPLE
+    Test-Obsolete -Mode 0
 
-        This example shows how to use the obsolete parameter. The Mode parameter is deprecated and will be removed in a future version.
+    This example shows how to use the obsolete parameter. The Mode parameter is deprecated and will be removed in a future version.
 
-        .NOTES
-        Author: Sam Erde
-        Version: 0.0.1
-        Modified: 2025-02-12
+    .NOTES
+    Author: Sam Erde
+    Version: 0.0.1
+    Modified: 2025-02-12
     #>
-
     [CmdletBinding()]
-
     param (
         # Test something
-        [Parameter()]
-        [Obsolete("Mode is being replaced by the more flexible 'Scans', 'OutputType', and 'IncludeFixes' parameters, and will be removed in a future version. Use 'Get-Help Test-Obsolete -Full' for more information.")]
+        [Parameter(Mandatory)]
+        [Obsolete("'Mode' is being replaced by the more flexible 'Scans', 'OutputType', and 'IncludeFixes' parameters, and will be removed in a future version. Use 'Get-Help Test-Obsolete -Full' for more information.")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateRange(0, 5)]
         [int16]
-        $Mode = 0
+        $Mode
     )
 
-    begin {
+    Write-Output "You chose mode ${Mode}."
 
-    } # end begin block
-
-    process {
-        Write-Output "You chose mode ${Mode}."
-    } # end process block
-
-    end {
-
-    } # end end block
-
-} # end function Test-Function
+} # end function Test-Obsolete
