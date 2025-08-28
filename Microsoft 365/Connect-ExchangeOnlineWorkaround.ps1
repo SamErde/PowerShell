a5d797ffadc4edcef6c4d40aac0699d301093174
Sam Erde <samuel.erde@gmail.com>
2025-08-28 09:17:23 -0400

    ✨feat: add Connect-ExchangeOnlineWorkaround function for token acquisition and connection to Exchange Online

---
+++ Microsoft 365/Connect-ExchangeOnlineWorkaround.ps1
@@ -0,0 +1,15 @@
+﻿function Connect-ExchangeOnlineWorkaround {
+    # Get a token and connect to Exchange Online
+    # Inspired by https://david-homer.blogspot.com/2025/01/exchange-online-management-powershell.html
+
+    <# To Do:
+    - Choose different environments to connect to
+    - Add error handling
+    #>
+    $ExoMsalPath = [System.IO.Path]::GetDirectoryName((Get-InstalledModule -Name 'ExchangeOnlineManagement').Path)
+    Add-Type -Path "$ExoMsalPath\Microsoft.IdentityModel.Abstractions.dll"
+    Add-Type -Path "$ExoMsalPath\Microsoft.Identity.Client.dll"
+    [Microsoft.Identity.Client.IPublicClientApplication] $ExoApplication = [Microsoft.Identity.Client.PublicClientApplicationBuilder]::Create('fb78d390-0c51-40cd-8e17-fdbfab77341b').WithDefaultRedirectUri().Build()
+    $ExoAuthToken = $ExoApplication.AcquireTokenInteractive([string[]]'https://outlook.office365.com/.default').ExecuteAsync().Result
+    Connect-ExchangeOnline -AccessToken $ExoAuthToken.AccessToken -UserPrincipalName $ExoAuthToken.Account.Username
+}
